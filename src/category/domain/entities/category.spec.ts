28ef00e6893dc7aabac72cb6c820e90a3375f663
Marcus Balbi <balbimarcus@gmail.com>
2022-08-17 22:14:12 -0300

    playing with jest and Category

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,7 +2,16 @@ import { Category } from "./category"
 
 describe('Category test', () => {
   test('constructor category', () => {
-    const c = new Category('Movie');
-    expect(c.name).toBe('Movie')
+    const c = new Category('Movie', 'random desc');
+    expect(c.name).toBe('Movie');
+    expect(c.description).toBe("random desc");
+    expect(c.active()).toBe(false);
+  })
+
+  test('create from props', () => {
+    const c = Category.createFromProps({ name: 'test', description: 'random desc',  is_active: true, created_at: new Date(1985, 1, 1) });
+    expect(c.name).toBe('test')
+    expect(c.description).toBe("random desc");
+    expect(c.active()).toBe(true);
   })
 })
--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,5 +1,27 @@
-export class Category {
-  constructor(readonly name: string) {
 
+export type CategoryProps = {
+  name: string;
+  description: string;
+  is_active?: boolean;
+  created_at?: Date;
+};
+export class Category {
+  private is_active: boolean;
+  private created_at: Date | null;
+
+  constructor(readonly name: string, readonly description: string) {
+    this.is_active = true;
+    this.created_at = new Date();
+  }
+
+  active (): boolean {
+    return this.is_active;
+  }
+
+  public static createFromProps(props: CategoryProps): Category {
+    const cat = new Category(props.name, props.description);
+    cat.is_active = props.is_active || false;
+    cat.created_at = props.created_at || null;
+    return cat;
   }
 }
