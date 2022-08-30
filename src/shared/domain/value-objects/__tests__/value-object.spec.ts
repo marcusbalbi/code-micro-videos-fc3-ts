a37ce9b9c272fe7133a2dee73c8b91e2efb75c46
Marcus Balbi <balbimarcus@gmail.com>
2022-08-29 21:02:13 -0300

    pretty

--- src/shared/domain/value-objects/__tests__/value-object.spec.ts
+++ src/shared/domain/value-objects/__tests__/value-object.spec.ts
@@ -1,4 +1,8 @@
-import { ComplexValueObject, SimpleValueObject, ValueObject } from "../value-object";
+import {
+  ComplexValueObject,
+  SimpleValueObject,
+  ValueObject,
+} from "../value-object";
 
 class SimpleStub extends SimpleValueObject {}
 class ComplexStub extends ComplexValueObject {}
@@ -16,24 +20,27 @@ class AddComplexStub extends ValueObject<Address> {
 describe("Value Object tests", () => {
   it("should have a get value", () => {
     const c = new SimpleStub("a");
-    expect(c.value).toBe("a")
+    expect(c.value).toBe("a");
   });
   it("should be comparable", () => {
     const c = new SimpleStub("a");
     const d = new SimpleStub(c.toString());
     expect(c.equals(d)).toBeTruthy();
   });
-  it('should work with an object', () => {
+  it("should work with an object", () => {
     const c = new ComplexStub({ a: "b" });
     expect(c.value).toEqual({ a: "b" });
-  })
-  it('should work with a Date', () => {
+  });
+  it("should work with a Date", () => {
     const d1 = new Date();
     const c = new ComplexStub(d1);
     expect(c.value).toEqual(d1);
-  })
-  it('should work with an interface/type', () => {
-    const c = new AddComplexStub({ city: "Rio de Janeiro", street: "Concordia"});
+  });
+  it("should work with an interface/type", () => {
+    const c = new AddComplexStub({
+      city: "Rio de Janeiro",
+      street: "Concordia",
+    });
     expect(c.value).toEqual({ city: "Rio de Janeiro", street: "Concordia" });
-  })
+  });
 });
