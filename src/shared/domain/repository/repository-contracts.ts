4361082ed5f0a4b999f13848a5626287ca679c3b
Marcus Balbi <balbimarcus@gmail.com>
2022-10-11 20:32:17 -0300

    SearchResult

--- src/shared/domain/repository/repository-contracts.ts
+++ src/shared/domain/repository/repository-contracts.ts
@@ -97,11 +97,55 @@ export class SearchParams {
   }
 }
 
-export class Search {}
+type SearchResultProps<E extends Entity,Filter> = {
+  items: E[];
+  total: number;
+  current_page: number;
+  per_page: number;
+  sort: string | null;
+  sort_dir: string | null;
+  filter: Filter;
+};
+export class SearchResult<E extends Entity,Filter = string> {
+  readonly items: E[];
+  readonly total: number;
+  readonly current_page: number;
+  readonly per_page: number;
+  readonly last_page: number;
+  readonly sort: string | null;
+  readonly sort_dir: string | null;
+  readonly filter: Filter | null;
+
+  constructor(props: SearchResultProps<E, Filter>) {
+    this.items = props.items;
+    this.total = props.total;
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.sort = props.sort;
+    this.sort_dir = props.sort_dir;
+    this.filter = props.filter;
+    this.last_page = Math.ceil(this.total / this.per_page)
+  }
+
+  toJSON () {
+    return {
+      items: this.items,
+      total: this.total,
+      current_page: this.current_page,
+      per_page: this.per_page,
+      sort: this.sort,
+      sort_dir: this.sort_dir,
+      filter: this.filter,
+      last_page: Math.ceil(this.total / this.per_page),
+    }
+  }
+
+}
 
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchOutput,
+  Filter = string,
+  SearchOutput =  SearchResult<E, Filter>,
   SearchInput = SearchParams
 > extends RepositoryInterface<E> {
   search(props: SearchInput): Promise<SearchOutput>;
