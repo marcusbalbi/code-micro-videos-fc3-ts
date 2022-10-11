a129ccb8df08f63c3659106194aec75b230bb46b
Marcus Balbi <balbimarcus@gmail.com>
2022-10-11 20:41:48 -0300

    SearchResult tests

--- src/shared/domain/repository/repository-contracts.spec.ts
+++ src/shared/domain/repository/repository-contracts.spec.ts
@@ -1,4 +1,4 @@
-import { SearchParams } from "./repository-contracts";
+import { SearchParams, SearchResult } from "./repository-contracts";
 
 describe("RepositoryContracts", () => {
   describe("SearchParams", () => {
@@ -113,10 +113,79 @@ describe("RepositoryContracts", () => {
         { filter: "aaa", expected: "aaa" },
       ];
       arrange.forEach((i) => {
-        expect(
-          new SearchParams({ filter: i.filter as any }).filter
-        ).toEqual(i.expected);
+        expect(new SearchParams({ filter: i.filter as any }).filter).toEqual(
+          i.expected
+        );
       });
+    });
+  });
+  describe("SearchResult", () => {
+    test("constructor props", () => {
+      let result = new SearchResult({
+        items: ["entity 1", "entity 2"] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 2,
+        sort: null,
+        sort_dir: null,
+        filter: null,
+      });
+      expect(result.toJSON()).toStrictEqual({
+        items: ["entity 1", "entity 2"] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 2,
+        sort: null,
+        sort_dir: null,
+        filter: null,
+        last_page: 2,
+      });
+
+      result = new SearchResult({
+        items: ["entity 1", "entity 2"] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 2,
+        sort: "name",
+        sort_dir: "desc",
+        filter: "test",
+      });
+      expect(result.toJSON()).toStrictEqual({
+        items: ["entity 1", "entity 2"] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 2,
+        sort: "name",
+        sort_dir: "desc",
+        filter: "test",
+        last_page: 2,
+      });
+    });
+
+    it("should set last page 1 when per_page is greater than total field", () => {
+      const result = new SearchResult({
+        items: ["entity 1", "entity 2"] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 15,
+        sort: null,
+        sort_dir: null,
+        filter: null,
+      });
+      expect(result.last_page).toEqual(1);
+    });
+
+    test("last_page prop when total is not a multiple of per_page", () => {
+      const result = new SearchResult({
+        items: ["entity 1", "entity 2"] as any,
+        total: 101,
+        current_page: 1,
+        per_page: 20,
+        sort: null,
+        sort_dir: null,
+        filter: null,
+      });
+      expect(result.last_page).toEqual(6);
     });
   });
 });
