6c938f4ed0d20478977353f0e2306c344a560848
Marcus Balbi <balbimarcus@gmail.com>
2022-10-17 19:58:39 -0300

    apply paginate

--- src/shared/domain/repository/in-memory.repository.ts
+++ src/shared/domain/repository/in-memory.repository.ts
@@ -93,9 +93,14 @@ export abstract class InMemorySearchableRepository<E extends Entity>
       return 0;
     })
   }
-  protected abstract applyPaginate(
+  protected async applyPaginate(
     items: E[],
     page: SearchParams["page"],
     per_page: SearchParams["per_page"]
-  ): Promise<E[]>;
+  ): Promise<E[]> {
+    const start = (page -1) * per_page;
+    const limit = start + per_page;
+
+    return items.slice(start, limit);
+  }
 }
