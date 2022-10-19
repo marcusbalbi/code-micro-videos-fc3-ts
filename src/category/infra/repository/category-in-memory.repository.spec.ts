2c22801972592ea9145cbd074b764b2449f38684
Marcus Balbi <balbimarcus@gmail.com>
2022-10-18 21:03:50 -0300

    improve tests

--- src/category/infra/repository/category-in-memory.repository.spec.ts
+++ src/category/infra/repository/category-in-memory.repository.spec.ts
@@ -15,8 +15,10 @@ describe("CategoryInMemoryRepository unit tests", () => {
     ];
     repository.items = items;
     const result = await repository.search(new CategorySearchParams());
+    const result2 = await repository.search(new CategorySearchParams({ sort: null, sort_dir: null }));
 
     expect(result.items).toStrictEqual([items[1], items[0]]);
+    expect(result2.items).toStrictEqual([items[1], items[0]]);
   });
   test("sort by name", async () => {
     const items = [
@@ -25,20 +27,34 @@ describe("CategoryInMemoryRepository unit tests", () => {
     ];
     repository.items = items;
     const result = await repository.search(new CategorySearchParams({ sort: 'name' }));
+    const result2 = await repository.search(new CategorySearchParams({ sort: 'name', sort_dir: 'desc' }));
 
     expect(result.items).toStrictEqual([items[1], items[0]]);
+    expect(result2.items).toStrictEqual([items[0], items[1]]);
   });
   test("filter by start", async () => {
     const items = [
       new Category({ name: "Category A", created_at: new Date("2022-01-05") }),
-      new Category({ name: "B Category", created_at: new Date("2022-01-02") }),
+      new Category({ name: "CaTeGoRy B", created_at: new Date("2022-01-05") }),
+      new Category({ name: "C Category", created_at: new Date("2022-01-05") }),
     ];
     repository.items = items;
     const result = await repository.search(
       new CategorySearchParams({ filter: "Category" })
     );
 
-    expect(result.items.length).toBe(1);
-    expect(result.items).toStrictEqual([items[0]]);
+    expect(result.items.length).toBe(2);
+    expect(result.items).toStrictEqual([items[0], items[1]]);
   });
+  test('null filter returns items', async () => {
+    const items = [
+      new Category({ name: "Category A" }),
+      new Category({ name: "Category B" }),
+    ];
+    repository.items = items;
+    const result = await repository.search(
+      new CategorySearchParams({ filter: null })
+    );
+    expect(result.items).toStrictEqual(items);
+  })
 });
