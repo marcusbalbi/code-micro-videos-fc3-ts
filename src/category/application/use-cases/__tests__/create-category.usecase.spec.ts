7567bd21830d832f3997322da2cac80e3d3ab395
Marcus Balbi <balbimarcus@gmail.com>
2022-10-25 19:22:25 -0300

    use case test

---
+++ src/category/application/use-cases/__tests__/create-category.usecase.spec.ts
@@ -0,0 +1,38 @@
+import { CategoryInMemoryRepository } from "../../../infra/repository/category-in-memory.repository";
+import CreateCategoryUseCase from "../create-category.usecase"
+
+describe("CreateCategoryUseCase unit test", () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  test('create a category', async () => {
+    const spy = jest.spyOn(repository, 'insert');
+    let output = await useCase.execute({ name: 'test' });
+    expect(output).toStrictEqual({
+      id: repository.items[0].id,
+      created_at: repository.items[0].created_at,
+      name: 'test',
+      description: undefined,
+      is_active: true,
+    });
+    expect(spy).toHaveBeenCalledTimes(1);
+
+    output = await useCase.execute({ name: 'test', description: 'desc', is_active: false });
+    expect(output).toStrictEqual({
+      id: repository.items[1].id,
+      created_at: repository.items[1].created_at,
+      name: 'test',
+      description: 'desc',
+      is_active: false,
+    });
+    expect(spy).toHaveBeenCalledTimes(2);
+  });
+
+
+
+})
