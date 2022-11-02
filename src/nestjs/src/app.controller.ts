c0bbe4f49f0aadcb245f49076999825df6ea9356
Marcus Balbi <balbimarcus@gmail.com>
2022-11-01 21:46:24 -0300

    init subpaths and monorepo

--- src/nestjs/src/app.controller.ts
+++ src/nestjs/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Category } from '@balbi/core/dist/category/domain/entities/category';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    const c = new Category({ name: 'JHgon' });
+    console.log(c);
     return this.appService.getHello();
   }
 }
